ef136637b9e262a3bea2b62b6807cee89be90ad5
dyjeon <dyjeon@seadronix.com>
2023-02-24 10:45:28 +0900

    materials: readthrough 2

--- learn-opengl/2.lightings/2.materials/shader/3.colors.fs
+++ learn-opengl/2.lightings/2.materials/shader/3.colors.fs
@@ -21,8 +21,6 @@ in vec3 FragPos;
 in vec2 TexCoord;
   
 uniform vec3 lightPos;
-uniform vec3 objectColor;
-uniform vec3 lightColor;
 uniform vec3 viewPos;
 
 uniform sampler2D texture1;
@@ -30,20 +28,20 @@ uniform sampler2D texture1;
 void main()
 {
     //amb set
-    vec3 ambient = lightColor * material.ambient;
+    vec3 ambient = light.ambient * material.ambient;
 
     //diffuse set
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(lightPos - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = (diff * material.diffuse) * lightColor;
+    vec3 diffuse = light.diffuse * (diff * material.diffuse);
 
     //specular set
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    vec3 specular = (material.specular * spec) * lightColor;
+    vec3 specular = light.specular * (material.specular * spec);
 
-    vec3 result = (specular + diffuse + ambient);
+    vec3 result = specular + diffuse + ambient;
     FragColor = vec4(result, 1.0);
 }
