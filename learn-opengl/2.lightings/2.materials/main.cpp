abe2f23e74d1056c9a51ea1677ee41e1574015ec
dyjeon <dyjeon@seadronix.com>
2023-02-27 10:08:16 +0900

    emission map done

--- learn-opengl/2.lightings/2.materials/main.cpp
+++ learn-opengl/2.lightings/2.materials/main.cpp
@@ -124,6 +124,19 @@ int main()
         -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f
     };
 
+    glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f),
+        glm::vec3( 2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3( 2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3( 1.3f, -2.0f, -2.5f),
+        glm::vec3( 1.5f,  2.0f, -2.5f),
+        glm::vec3( 1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
+
     unsigned int VBO, cubeVAO;
     glGenVertexArrays(1, &cubeVAO);
     glGenBuffers(1, &VBO);
@@ -175,12 +188,10 @@ int main()
         cube_shader.use();
         light_pos.x = (sin(glfwGetTime()) + 1.f) / 2.f;
         light_pos.y = sin(glfwGetTime() / 2.f) * 1.f;
-        cube_shader.setVec3("light.position", light_pos);
+        // cube_shader.setVec3("light.position", light_pos);
+        cube_shader.setVec3("light.direction", -0.2f, -1.0f, -0.3f);
         cube_shader.setVec3("viewPos", cam.Position);
         
-        // cube_shader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
-        // cube_shader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
-        // cube_shader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
         cube_shader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
         cube_shader.setVec3("light.diffuse", 1.0f, 1.0f, 1.0f);
         cube_shader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
@@ -205,8 +216,18 @@ int main()
         glBindTexture(GL_TEXTURE_2D, ems_map);
 
         glBindVertexArray(cubeVAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
-        
+        for (unsigned int i = 0; i < 10; i++)
+        {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            model = glm::rotate(model, glm::radians(angle),
+                                glm::vec3(1.0f, 0.3f, 0.5f));
+            cube_shader.setMat4("model", model);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+
+        /*
         light_cube_shader.use();
         light_cube_shader.setMat4("projection", proj);
         light_cube_shader.setMat4("view", view);
@@ -217,6 +238,7 @@ int main()
 
         glBindVertexArray(lightCubeVAO);
         glDrawArrays(GL_TRIANGLES, 0, 36);
+        */
         
         glfwSwapBuffers(window);
         glfwPollEvents();
